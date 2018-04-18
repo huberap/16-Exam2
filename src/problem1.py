3d2139a6445f63b4fbad50a20266c8e4d3d417db
huberap <huberap@rose-hulman.edu>
2018-04-17 21:43:31 -0400

    DONE

--- src/problem1.py
+++ src/problem1.py
@@ -2,8 +2,8 @@
 Exam 2, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  April 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Alex Huber.  April 2018.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -12,7 +12,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.  READ the code of the  Rect  class below.
+# done 2.  READ the code of the  Rect  class below.
 #
 #   Once you are confident that you understand the  Rect  class and its code,
 #   change the TO-DO for this problem to DONE.
@@ -29,13 +29,23 @@ class Rect(object):
 def run_test_problem1():
     """ Tests the   problem1   function. """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement at least 2 tests of the  problem1  function.
+    # done: 3. Implement at least 2 tests of the  problem1  function.
     # -------------------------------------------------------------------------
     print()
     print('--------------------------------------------------')
     print('Testing the   problem1   function:')
     print('--------------------------------------------------')
 
+    rectangles = [Rect(1, 1), Rect(1, 1)]
+    print('Test 1')
+    print('expected = 2')
+    print('actual = ', problem1(rectangles))
+
+    rectangles2 = [Rect(5, 10), Rect(4, 3), Rect(100, 7)]
+    print()
+    print('Test 2')
+    print('expected = 762')
+    print('actual = ', problem1(rectangles2))
 
 def problem1(rectangles):
     """
@@ -53,9 +63,12 @@ def problem1(rectangles):
     :return: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # done: 4. Implement and test this function.
     # -------------------------------------------------------------------------
-
+    sumarea = 0
+    for k in range(len(rectangles)):
+        sumarea = sumarea + ((rectangles[k].w) * (rectangles[k].h))
+    return sumarea
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
